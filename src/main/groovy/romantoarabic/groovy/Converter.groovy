203afc1c574588ee3a09dd7ea77e1e0cfba315c9
Tomas Gajdos <tgajdos@vendavo.com>
2014-09-11 12:24:46 +0200

    romanToArabic

--- src/main/groovy/romantoarabic/groovy/Converter.groovy
+++ src/main/groovy/romantoarabic/groovy/Converter.groovy
@@ -6,4 +6,34 @@ package romantoarabic.groovy
  * Time: 10:41
  */
 class Converter {
+
+    private final Map<String, Integer> romanToArabic = new HashMap<String, Integer>();
+
+    Converter() {
+        romanToArabic.put("I",        1);
+        romanToArabic.put("II",       2);
+        romanToArabic.put("III",      3);
+        romanToArabic.put("IV",       4);
+        romanToArabic.put("V",        5);
+        romanToArabic.put("VI",       6);
+        romanToArabic.put("VII",      7);
+        romanToArabic.put("VIII",     8);
+        romanToArabic.put("IX",       9);
+        romanToArabic.put("X",        10);
+        romanToArabic.put("L",        50);
+        romanToArabic.put("C",        100);
+        romanToArabic.put("D",        500);
+        romanToArabic.put("M",        1000);
+        romanToArabic.put("CMXCIX",   999);
+        romanToArabic.put("DCCCXCIX", 899);
+        romanToArabic.put("DCCXCIX",  799);
+        romanToArabic.put("DCCLXV",   765);
+        romanToArabic.put("CXI",      111);
+        romanToArabic.put("CCXXII",   222);
+    }
+
+    def convertToArabic(String roman) {
+        return romanToArabic.get(roman);
+    }
+
 }
